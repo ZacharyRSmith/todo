44ac5a8e9cf364ad285a8153b342a2a24937a4a9
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-05-07 17:55:34 +0000

    #6 ActivityMethodTests for is_on_time().

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,25 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
+from .models import Activity, Task
 
-# Create your tests here.
+
+class ActivityMethodTests(TestCase):
+    """
+    Activity is the abstract model for Task and Subtask.
+    Using Task objects to test Activity.
+    """
+
+    def test_is_on_time_with_finished_task(self):
+        task = Task(is_finished = True)
+        self.assertEqual(task.is_on_time(), None)
+
+    def test_is_on_time_with_future_due_date(self):
+        future_date = timezone.now() + datetime.timedelta(days = 30)
+        task = Task(due_date = future_date)
+        self.assertEqual(task.is_on_time(), True)
+
+    def test_is_on_time_with_past_due_date(self):
+        past_date = timezone.now() - datetime.timedelta(days = 30)
+        task = Task(due_date = past_date)
+        self.assertEqual(task.is_on_time(), False)
